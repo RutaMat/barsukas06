7c01fcd76398922725118b92f768070fd2b06219
RutaMat <79446887+RutaMat@users.noreply.github.com>
2021-06-30 17:35:07 +0300

    visi obj ND done

---
+++ NamuDarbai/14NamuD Objektai/8/Index.php
@@ -0,0 +1,26 @@
+<?php
+
+require __DIR__ . '/Pinigine.php';
+
+
+$pinigine = new Pinigine;
+
+$pinigine->skaiciuoti();
+
+$pinigine->ideti(5);
+$pinigine->ideti(0.33);
+$pinigine->ideti(5);
+$pinigine->ideti(0.33);
+$pinigine->ideti(5);
+$pinigine->ideti(0.33);
+$pinigine->ideti(5);
+
+$pinigine->skaiciuoti();
+
+$pinigine->monetos();
+
+$pinigine->banknotai();
+
+echo '<pre>';
+var_dump($pinigine);
+© 2021 GitHub, Inc.
